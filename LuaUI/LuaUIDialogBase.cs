ff786459331264cf3d01b79a48bf759aad8eafc2
freeyxm <freeyxm@gamil.com>
2015-07-02 10:56:23 +0800

    update LuaBinder

--- LuaUI/LuaUIDialogBase.cs
+++ LuaUI/LuaUIDialogBase.cs
@@ -7,9 +7,9 @@ using LuaInterface;
 
 namespace LuaUtility
 {
-    public class LuaBinder : MonoBehaviour
+    public class LuaUIDialogBase : UIDialogBase
     {
-        public string m_name;
+        public string UIName = "LuaUIDialogBase";
         public string m_luaFile;
         public GameObject[] m_gameObjects;
 
@@ -17,7 +17,7 @@ namespace LuaUtility
         private Dictionary<string, object> m_objects;
         private bool m_luaLoaded = false;
 
-        void Init()
+        void Initialize()
         {
             m_luaHelper = new LuaHelper(false); // It's important to use a non global lua state, or lua function belong to different script will confused.
             m_objects = new Dictionary<string, object>();
@@ -30,19 +30,26 @@ namespace LuaUtility
                 if (!m_objects.ContainsKey(gameObject.name))
                     m_objects.Add(gameObject.name, gameObject);
             }
-
-            if (!string.IsNullOrEmpty(m_name))
-            {
-                LuaBinderMgr.Instance.AddObject(m_name, this);
-            }
         }
 
-        void Awake()
+        protected override void Init()
+        {
+            InitMediator(new LuaUIDialogBaseMediator(this));
+        }
+
+        public override string GetUIName()
+        {
+            return UIName;
+        }
+
+        new void Awake()
         {
             // Can't bind LuaBinder to gameObject in lua script --> Awake called befor property be setted.
-            Init();
+            Initialize();
             
             LoadLua();
+
+            base.Awake();
 
             CallLuaFunction("Awake");
         }
@@ -52,8 +59,9 @@ namespace LuaUtility
             CallLuaFunction("Start");
         }
 
-        void OnDestroy()
+        new void OnDestroy()
         {
+            base.OnDestroy();
             CallLuaFunction("OnDestroy");
         }
 
@@ -67,10 +75,58 @@ namespace LuaUtility
             CallLuaFunction("OnDisable");
         }
 
+        public override void RefreshUI()
+        {
+            base.RefreshUI();
+            CallLuaFunction("RefreshUI");
+        }
+
+        protected override void OnPlayOpenAnimBegine()
+        {
+            base.OnPlayOpenAnimBegine();
+            CallLuaFunction("OnPlayOpenAnimBegine");
+        }
+
+        protected override void OnPlayOpenAnimEnd()
+        {
+            base.OnPlayOpenAnimEnd();
+            CallLuaFunction("OnPlayOpenAnimEnd");
+        }
+
+        protected override void OnPlayCloseAnimBegine()
+        {
+            base.OnPlayCloseAnimBegine();
+            CallLuaFunction("OnPlayCloseAnimBegine");
+        }
+
+        protected override void OnPlayCloseAnimEnd()
+        {
+            base.OnPlayCloseAnimEnd();
+            CallLuaFunction("OnPlayCloseAnimEnd");
+        }
+
+        public override void OnFocusStateChange(bool gotFocus, UIPanelBase ui)
+        {
+            base.OnFocusStateChange(gotFocus, ui);
+            CallLuaFunction("OnFocusStateChange", gotFocus, ui);
+        }
+
         void LoadLua()
         {
             if (!m_luaLoaded && !string.IsNullOrEmpty(m_luaFile))
             {
+                m_luaHelper.DoFile("CommonImport.lua");
+
+                //StringBuilder buff = new StringBuilder();
+                //buff.Append(UIName).Append(" = {};").AppendLine();
+                //buff.Append("function ").Append(UIName).Append(":new()").AppendLine();
+                //buff.Append("    local o = {};").AppendLine();
+                //buff.Append("    setmetatable(o, self);").AppendLine();
+                //buff.Append("    self.__index = self;").AppendLine();
+                //buff.Append("    return o;").AppendLine();
+                //buff.Append("end").AppendLine();
+                //m_luaHelper.DoString(buff.ToString());
+
                 m_luaHelper.DoFile(m_luaFile);
                 m_luaLoaded = true;
             }
@@ -108,6 +164,11 @@ namespace LuaUtility
                 return m_objects[key];
             else
                 return null;
+        }
+
+        public void SendNotification(string name, object value)
+        {
+            GameManager.Instance.SendNotification(name, value);
         }
     }
 }
---
+++ LuaUI/LuaUIDialogBaseMediator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LuaUtility
+{
+    public class LuaUIDialogBaseMediator : BaseCommMediator<LuaUIDialogBase>
+    {
+        public LuaUIDialogBaseMediator(LuaUIDialogBase ui)
+            : base(ui.UIName + "Mediator", ui)
+        {
+            viewUI.CallLuaFunction("Mediator_Init", this);
+        }
+
+        public override void HandleNotification(PureMVC.Interfaces.INotification notification)
+        {
+            //Debuger.Log(string.Format("HandleNotification: {0} -> {1}", notification.Name, notification.Body));
+            viewUI.CallLuaFunction("Mediator_HandleNotification", notification.Name, notification.Body);
+        }
+
+        public void AddNotify(string notify)
+        {
+            //Debuger.Log(string.Format("AddNotify: {0}", notify));
+            BaseCommMediator<LuaUIDialogBase>.AddNotify(notify);
+        }
+    }
+}
